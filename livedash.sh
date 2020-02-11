783329c887b8f2c78db98973a1cfb09340724e49
Alex Lambert <alexlambert@fb.com>
2020-02-11 14:41:22 -0300

    .

--- livedash.sh
+++ livedash.sh
@@ -3,6 +3,9 @@
 set -e
 set -x
 
+scenario=standard
+port=8080
+
 out=`mktemp -d`
 
 cd "${out}"
@@ -18,20 +21,40 @@ cd "${out}"
 # but we need CORS, so follow https://stackoverflow.com/a/28632834:
 # npm install http-server
 # make sure you use `-c-1` to disable caching, or you'll serve old manifests :-(
-~/node_modules/http-server/bin/http-server . -p 8080 --cors -c-1 &
+~/node_modules/http-server/bin/http-server . -p "${port}" --cors -c-1 &
 
 httpd_pid=$!
 
 # On Mac:
 # https://trac.ffmpeg.org/wiki/CompilationGuide/macOS#CompilingFFmpegyourself
-# git clone https://git.ffmpeg.org/ffmpeg.git
+# git clone https://github.com/alambert/ffmpeg
 # ./configure --enable-gpl --enable-libx264 --enable-filter=drawtext  --enable-libfreetype
 # make
 
 # On Ubuntu:
-# git clone https://git.ffmpeg.org/ffmpeg.git
+# git clone https://github.com/alambert/ffmpeg
 # sudo apt install gcc yasm libfreetype6-dev pkg-config libx264-dev ttf-dejavu
 # ./configure --enable-gpl --enable-libx264 --enable-filter=drawtext  --enable-libfreetype
+
+case "${scenario}" in
+standard)
+  scenario_args='-use_timeline 0 -init_seg_name "init-stream$RepresentationID$.mp4" -media_seg_name "chunk-stream$RepresentationID$-$Number$.mp4"'
+  ;;
+timeline-crash)
+  scenario_args='-use_timeline 1 -init_seg_name "init-stream$RepresentationID$.mp4" -media_seg_name "chunk-stream$RepresentationID$-$Number$.mp4"'
+  ;;
+relative-path)
+  scenario_args='-use_timeline 0 -init_seg_name "foobar/../init-stream$RepresentationID$.mp4" -media_seg_name "foobar/../chunk-stream$RepresentationID$-$Number$.mp4"'
+  ;;
+*)
+  echo "${0}: unknown scenario ${scenario}"
+  exit 1
+  ;;
+esac
+
+#fontfile="/Library/Fonts/Roboto-Thin.ttf"
+fontfile="/usr/share/fonts/truetype/ttf-dejavu/DejaVuSansMono.ttf"
+
 ~/w/ffmpeg/ffmpeg \
   -hide_banner \
   -re \
@@ -39,7 +62,7 @@ httpd_pid=$!
   -i "testsrc2=size=1280x720:rate=30,format=yuv420p" \
   -f lavfi \
   -i "sine=frequency=1000:sample_rate=44100" \
-  -vf "drawtext=fontfile=/usr/share/fonts/truetype/ttf-dejavu/DejaVuSansMono.ttf:text='%{gmtime}Z %{n}':box=1:fontcolor=black:fontsize=64:x=(w-tw)/2:y=(h-th)/2" \
+  -vf "drawtext=fontfile=${fontfile}:text='%{gmtime}Z %{n}':box=1:fontcolor=black:fontsize=64:x=(w-tw)/2:y=(h-th)/2" \
   -c:v libx264 \
   -preset:v ultrafast \
   -profile:v high \
@@ -55,13 +78,8 @@ httpd_pid=$!
   -window_size 150 \
   -extra_window_size 60 \
   -remove_at_exit 1 \
-  -use_timeline 1 \
-  -init_seg_name 'init-stream$RepresentationID$.mp4' \
-  -media_seg_name 'chunk-stream$RepresentationID$-$Number$.mp4' \
+  ${scenario_args}
   manifest.mpd &
-
-#  -use_timeline 0 \
-#   -vf "drawtext=fontfile=/Library/Fonts/Roboto-Thin.ttf:text='%{gmtime}Z %{n}':box=1:fontcolor=black:fontsize=24:x=(w-tw)/2:y=(h-th)/2" \
 
 ffmpeg_pid=$!
 
